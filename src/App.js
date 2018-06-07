b1f1a625d58f5d990c7ad9ffdeba154a97b2b4b8
champ <thechamp2535@gmail.com>
2018-06-07 15:21:07 +0700

    Edit countTodo function

--- src/App.js
+++ src/App.js
@@ -14,26 +14,11 @@ class App extends Component {
         {id:3,text:'Walk the cat',completed:false}
 
     ],
-    counttodo : 3,
     status: 'All'
   };
-  incrementCount = () => {
-    this.setState(() => {
-    return this.setState({counttodo: this.state.todos.length});
-    });
-  };
-  reducedCount = () => {
-    var count = this.state.todos.length - 1;
-    this.setState(() => {
-      return this.setState({counttodo: count});
-      });
-  }
   handClick = () =>{
     this.setState({
-        todos:[
-            // {id:1,text:'New todos'},
-        ],
-        counttodo: 0
+        todos:[]
     });
   };
 
@@ -48,7 +33,6 @@ class App extends Component {
     const currentTodes = this.state.todos;
     
     currentTodes.push({id:newId1,text:newtext});
-    this.incrementCount();
     this.setState({
       todos:currentTodes
     });
@@ -61,31 +45,11 @@ class App extends Component {
     const newtodo = currentTodes.filter((todo)=>{
       return todo.id !== id;
     });
-    this.reducedCount();
+    // this.reducedCount();
     this.setState({
       todos:newtodo
     });
-  }
-
-  // setcompleted = (id) =>{
-  //   const currentTodes = this.state.todos;
-    
-  //   const newtodo = currentTodes.map((todo)=>{
-  //     let tododata = todo;
-  //     if(tododata.id === id){
-  //       if(tododata.completed){
-  //         tododata.completed = false;
-  //       }else{
-  //         tododata.completed = true;
-  //       }
-  //     }
-  //     return tododata;
-  //   });
-  //   this.setState({
-  //     todos:newtodo
-  //   });
-
-  // };
+  };
 
   toggleComplete = (id,completed) =>{
     const currentTodes = this.state.todos;
@@ -101,6 +65,10 @@ class App extends Component {
     this.setState({
       status: newstatus
     });
+  };
+
+  countTodo = () =>{
+    return this.state.todos.length;
   }
 
   render() {
@@ -108,7 +76,7 @@ class App extends Component {
       <div className="todoapp">
         <Header onSubmit={this.addTodo}/>
         <Main todos={this.state.todos} onReset={this.handClick} ondelete={this.deleteTodo} oncompleted={this.toggleComplete} status={this.state.status} />
-        <Footer count={this.state.counttodo} status={this.state.status} onStatus={this.setStatus} />
+        <Footer count={this.countTodo} status={this.state.status} onStatus={this.setStatus} />
       </div>
     );
   }
